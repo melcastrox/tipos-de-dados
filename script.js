9bca97147916dbd45187ad328cc6615743bf1c46
Melyssa <melcastro417@gmail.com>
2025-04-02 18:27:53 -0300

    tipo de dados

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const nome = "Melyssa"; // String
+const idade = 19; // Number
+const estudante = true; // Boolean
+const hobbies = ["Ler", "Programar", "Jogar"]; // Array
+const endereco = { rua: "Rua das Flores", numero: 123, cidade: "São Paulo" }; // Objeto
+
+console.log(typeof nome); // string
+console.log(typeof idade); // number
+console.log(typeof estudante); // boolean
+console.log(typeof hobbies); // object (arrays são do tipo object em JavaScript)
+console.log(typeof endereco); // object
+
+const nnome = "Melyssa"
+const iidade = "19"
+
+
+console.log(`Olá, eu me chamo ${nome.toUpperCase()} e tenho ${idade} anos.`);
